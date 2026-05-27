a0f451c2e1a00b076a452e19974e0951d1958bd4
thehavlok <thehavlok@gmail.com>
2026-05-27 16:07:23 +0300

    change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Multer config for file uploads
 const storage = multer.diskStorage({
